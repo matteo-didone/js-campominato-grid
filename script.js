b955af464c4678af3b19e14cd57f3a63f4491386
matteo-didone <matteo.dido9@gmail.com>
2023-05-23 18:37:38 +0200

    Added ids to options

--- script.js
+++ script.js
@@ -1,44 +1,80 @@
 // The user clicks on the button "Play"
-//That will generate a square game grid
-//Each cell has a progressive number from 1 to 100
-//There will be 10 cells for each of the 10 rows
-//When the user clicks on each cell, the clicked cell turns blue, and a message is emitted in the console with the number of the clicked cell. 
+// That will generate a square game grid
+// Each cell has a progressive number from 1 to 100
+// There will be 10 cells for each of the 10 rows
+// When the user clicks on each cell, the clicked cell turns blue, and a message is emitted in the console with the number of the clicked cell. 
+
+// BONUS: 
+// for difficulty 1 => 100 cells, with a number ranging from 1 to 100, divided into 10 cells per 10 rows;
+// for difficulty 2 => 81 cells, with a number ranging from 1 to 81, divided into 9 cells per 9 rows;
+// for difficulty 3 => 49 cells, with a number ranging from 1 to 49, divided into 7 cells per 7 rows.
 
 // The play button is stored into a JS variable
 const playButton = document.getElementById('btn-play');
+// The selector is stored into a JS variable
+const selector = document.getElementById('difficulty');
+// The difficulty options need to be stored into a JS variable too
+const difficultyOne = document.getElementById('difficulty-one');
+const difficultyTwo = document.getElementById('difficulty-two');
+const difficultyThree = document.getElementById('difficulty-three');
 
 // Add click event listener to the play button
 playButton.addEventListener('click', function() 
 {
-    // The game grid is generated
-    generateGrid();
+    // Get the selected difficulty level
+    const difficultyString = selector.value;
+    const difficultyLevel = parseInt(difficultyString.match(/\d+/) || 0);;
+
+    // Generate the game grid based on the selected difficulty level
+    generateGrid(difficultyLevel);
 });
 
 // Game grid function method
-function generateGrid() 
+function generateGrid(difficultyLevel) 
 {
     // The game grid is generated
     const grid = document.getElementById('grid');
     // Clear the grid
     grid.innerHTML = '';
 
-    // Create 10 rows
-    for (let i = 0; i < 10; i++) 
+    // Define the number of cells and rows based on the difficulty level
+    let numCells, numRows;
+    if (difficultyLevel === 1) 
+    {
+        numCells = 100;
+        numRows = 10;
+    } 
+    else if (difficultyLevel === 2) 
+    {
+        numCells = 81;
+        numRows = 9;
+    } 
+    else if (difficultyLevel === 3) 
+    {
+        numCells = 49;
+        numRows = 7;
+    }
+
+    // Calculate the number of cells per row
+    const numCellsPerRow = numCells / numRows;
+
+    // Create rows
+    for (let i = 0; i < numRows; i++) 
     {   
         // Create a row element
         const row = document.createElement('div');
         // Add row class to the row element
         row.classList.add('row');
 
-        // Create 10 cells for each row
-        for (let j = 0; j < 10; j++) 
+        // Create cells for each row
+        for (let j = 0; j < numCellsPerRow; j++) 
         {
             // Create a cell element
             const cell = document.createElement('div');
-            // Add cell class to teh cell element
+            // Add cell class to the cell element
             cell.classList.add('cell');
             // Add text to the cell
-            cell.innerText = i * 10 + j + 1;
+            cell.innerText = i * numCellsPerRow + j + 1;
             // Append the cell to the row
             row.appendChild(cell);
 
@@ -56,5 +92,3 @@ function generateGrid()
         grid.appendChild(row);
     }
 }
-
-
